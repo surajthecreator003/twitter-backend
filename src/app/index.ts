c310bf7cc592fcca19f7dde3524fd84457047b64
Suraj Mallick <surajhustlermallick@gmail.com>
2024-01-25 16:31:52 +0530

    added typecript and configed tscongif.json ,installed express and graphql and tested it

---
+++ src/app/index.ts
@@ -0,0 +1,44 @@
+import { ApolloServer } from "@apollo/server";
+import express from "express";
+import {expressMiddleware} from "@apollo/server/express4";
+import bodyParser from "body-parser";
+
+//the graphql server and the normal express server
+export async function initServer(){
+    const app=express();//create the express server
+
+
+   app.use(bodyParser.json());//json to normal js 
+
+
+    //creating the graphql Server
+    //typeDefs is like the Schema resolvers contains the Query and Mutation
+    //Query is for fetching some data from server
+    //while resolvers is for changing data on server 
+    const graphqlServer= new ApolloServer<any>(
+        {
+        typeDefs:`
+           type Query{
+                sayHello:String
+           },
+          
+        `,
+        resolvers:{
+                   Query:{
+                        sayHello:()=>`Hello From GraphQl Server`
+                   },
+                   
+                  }
+    }
+    )
+    
+
+
+    await graphqlServer.start();//start the graphql server
+
+    app.use("/graphql",expressMiddleware(graphqlServer))//pass on the graphql server to express server
+
+    return app;
+
+    };
+
